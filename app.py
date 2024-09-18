ae7a1598a0af6b77ac6f438b1b374fd4034563c3
AryanKukreti <96968671+AryanKukreti@users.noreply.github.com>
2024-09-19 04:56:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def generate():
     return jsonify({'plot_url': plot_url})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)  # Update to listen on all interfaces
