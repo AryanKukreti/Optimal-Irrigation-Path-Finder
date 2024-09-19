f1a1e709c4e825c1dc52b206f17defb406cfe973
AryanKukreti <96968671+AryanKukreti@users.noreply.github.com>
2024-09-19 06:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def start():
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
